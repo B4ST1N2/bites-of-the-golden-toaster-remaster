c3b9eefb05b61fec222173856b8a59b9a5bb6d5a
Santiago Sepulveda Montoya <sngiagom@gmail.com>
2025-08-28 00:28:19 -0500

    ♻️ Refactor bread character script

--- scenes/ss_bread.gd
+++ scenes/ss_bread.gd
@@ -6,14 +6,18 @@ var is_facing_rigth =  true
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta): 
-	if Input.is_action_just_pressed("jump"): 
-		velocity.y = -jump_speed 
-		
-	velocity.y += gravity * delta
-	
+	jump(delta)
 	move_x()
 	flip()
 	move_and_slide()
+
+func jump(delta):
+	if Input.is_action_just_pressed("jump") and is_on_floor(): 
+		velocity.y = -jump_speed 
+	
+	if not is_on_floor():	
+		velocity.y += gravity * delta
+	
 
 func flip():
 	if (is_facing_rigth and velocity.x < 0) or (not is_facing_rigth and velocity.x > 0): 
