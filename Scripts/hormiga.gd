055638aea49cbf61990f205d653a170eba54e411
VictorVidal12 <vvidal171@soyudemedellin.edu.co>
2025-10-28 15:41:36 -0500

    ⚡️Implement collision of ants
    Implement the collision hit in the ants

--- Scripts/hormiga.gd
+++ Scripts/hormiga.gd
@@ -9,15 +9,29 @@ func _ready() -> void:
 	
 func _process(delta: float) -> void:
 	move_character(delta)
+	turn()
 
 func move_character(delta: float) -> void:
 	velocity.y += gravity * delta
-	velocity.x = -speed
-	move_and_slide()
+	
+	if (moving_left):
+		velocity.x = -speed
+		move_and_slide()
+	else:
+		velocity.x = speed
+		move_and_slide()
+		
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.get_name() == "ss_bread":
 		body._loseLife()
 		pass
+		
+		
+func turn():
+	if not $RayCast2D.is_colliding() or $WallCheck.is_colliding():
+		moving_left = !moving_left
+		scale.x = -scale.x
+
 		
